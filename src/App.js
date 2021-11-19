c5f5d1497f388ad24f8f1d48b5f59b998373c0ef
Danielle-91 <danielle.alamilla@gmail.com>
2021-11-19 12:27:55 -0500

    initial commit and most html/styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Record + Play</h1>
       </header>
+
+      <main>
+        <section class="howTo">
+          <p>Need workout motivation? Cooking music? Something to play on a boombox outside your crush’s window in the middle of the night? We’ve got you covered.  
+          <span>Search any artist or genre and we’ll provide 10 songs to turn into your very own mixtape:</span>
+          </p>
+          <form action="submit">
+            <label for="username">Search Here:</label>
+            <input type="text" name="search" id="search"/>
+            <button>Search</button>
+          </form>
+        </section>
+
+        <section className="resultPage">
+          <h2>Your (artist/genre) Mixtape:</h2>
+        </section>
+      </main>
+      <footer>
+        <p>Created at <a href="https://junocollege.com/">Juno College of Technology</a></p>
+      </footer>
     </div>
   );
 }
