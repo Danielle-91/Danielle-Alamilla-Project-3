066c5e4e3587a883b3ae70a881117b19c79b6bb7
Danielle-91 <danielle.alamilla@gmail.com>
2021-11-23 14:39:11 -0500

    MVP Goals complete!

--- src/Mixtape.js
+++ src/Mixtape.js
@@ -7,13 +7,28 @@ function Mixtape({tracks}) {
                 <>
                     <h2>Your Mixtape:</h2>
                     <div className="results">
+                        <div className="tapeStyles">
+                            <div className="sideA">
+                                <p>A</p>
+                                <p>Date:</p>
+                                <p>N.R [ ] Yes [ ] No</p>
+
+                            </div>
+                            <div className="sideB">
+                                <p>B</p>
+                                <p>Date:</p>
+                                <p>N.R [ ] Yes [ ] No</p>
+                            </div>
+                            
+                        </div>
                         <div className="trackContainer">
                             <ul>
-                        {tracks.map((track) => {
-                            return(
-                                <li key={track.trackId}>{track.trackTitle}{track.artistName}</li>
-                                );
-                            })}
+                                {tracks.map((track) => {
+                                    console.log(track)
+                                return(
+                                    <li key={track.trackId}>{track.trackName}<span>{track.artistName}</span></li>
+                                    );
+                                })}
                             </ul>
                             </div>
                     </div>
